e325fbb1d9294dd4e806dcc4994fb5949baffab9
Jaejoon Han <jj2726@gmail.com>
2025-03-17 00:31:34 +0900

    에러 로깅 추가

--- src/TweetGenerator/Function.cs
+++ src/TweetGenerator/Function.cs
@@ -9,7 +9,6 @@ namespace TweetGenerator;
 
 public class Function(IConfiguration configuration, ILoggerFactory loggerFactory, OpenAIService openAiService, TweetService tweetService, SlackService slackService)
 {
-    private readonly List<Stock> _stocks = JsonSerializer.Deserialize<List<Stock>>(configuration["Stocks"] ?? "[]") ?? [];
     private readonly ILogger _logger = loggerFactory.CreateLogger<Function>();
 
     [Function("GenerateTweet")]
@@ -21,58 +20,68 @@ public class Function(IConfiguration configuration, ILoggerFactory loggerFactory
 #endif
         )
     {
-        _logger.LogDebug("get stock price using Yahoo Finance API at {time}", myTimer.ScheduleStatus?.Last ?? DateTime.UtcNow);
-
-        var stockInfo = await YahooFinanceService.GetPriceInfo([.. _stocks.Select(s => s.Symbol)]);
-
-        foreach (var stock in _stocks)
+        try
         {
-            _logger.LogInformation("[Stock: {stock}]", stock);
+            var stocks = JsonSerializer.Deserialize<List<Stock>>(configuration["Stocks"] ?? "[]") ?? [];
+            _logger.LogInformation("Starting tweet generation for {count} stocks at {now}", stocks.Count, DateTime.UtcNow);
 
-            var symbol = stock.Symbol;
-            var security = stockInfo[symbol];
+            _logger.LogDebug("get stock price using Yahoo Finance API at {time}", myTimer.ScheduleStatus?.Last ?? DateTime.UtcNow);
 
-            _logger.LogDebug($"validate");
+            var stockInfo = await YahooFinanceService.GetPriceInfo([.. stocks.Select(s => s.Symbol)]);
 
-            if (security.MarketState is "REGULAR")
+            foreach (var stock in stocks)
             {
-                _logger.LogInformation("Market is open");
-                return;
+                _logger.LogInformation("[Stock: {stock}]", stock);
+
+                var symbol = stock.Symbol;
+                var security = stockInfo[symbol];
+
+                _logger.LogDebug($"validate");
+
+                if (security.MarketState is "REGULAR")
+                {
+                    _logger.LogInformation("Market is open");
+                    return;
+                }
+
+                var marketTime = DateTimeOffset.FromUnixTimeSeconds(security.RegularMarketTime).UtcDateTime;
+                var estZone = TimeZoneInfo.FindSystemTimeZoneById("Eastern Standard Time");
+                var estNow = TimeZoneInfo.ConvertTimeFromUtc(DateTime.UtcNow, estZone);
+                var estMarketTime = TimeZoneInfo.ConvertTimeFromUtc(marketTime, estZone);
+
+                if (estMarketTime.Day != estNow.Day)
+                {
+                    _logger.LogInformation("Invalid Day");
+                    return;
+                }
+
+                _logger.LogInformation($"create image using DALL-E 3 API");
+
+                var prompt = openAiService.GetPrompt(security.RegularMarketChange > 0)
+                    .Replace("[stockName]", security.ShortName)
+                    .Replace("[currentPrice]", string.Format("{0:N2}", security.RegularMarketPrice))
+                    .Replace("[priceChange]", string.Format("{0:N2}", security.RegularMarketChange))
+                ;
+
+                var imageByte = await openAiService.CreateImage(prompt);
+
+                var content = $"""
+                [{estMarketTime:yyyy-MM-dd}]
+                the stock price of ${symbol}
+                ${string.Format("{0:N2}", security.RegularMarketPrice)}
+                {(security.RegularMarketChange > 0 ? "+" : "")}{string.Format("{0:N2}", security.RegularMarketChange)} ({string.Format("{0:N2}", security.RegularMarketChangePercent)}%)
+                """;
+
+                _logger.LogInformation("post tweet using X API");
+                await tweetService.PostTweet(content, imageByte);
+
+                _logger.LogInformation("send slack message using Slack API");
+                await slackService.SendMessage(stock.SlackChannel, symbol, content, imageByte);
             }
-
-            var marketTime = DateTimeOffset.FromUnixTimeSeconds(security.RegularMarketTime).UtcDateTime;
-            var estZone = TimeZoneInfo.FindSystemTimeZoneById("Eastern Standard Time");
-            var estNow = TimeZoneInfo.ConvertTimeFromUtc(DateTime.UtcNow, estZone);
-            var estMarketTime = TimeZoneInfo.ConvertTimeFromUtc(marketTime, estZone);
-
-            if (estMarketTime.Day != estNow.Day)
-            {
-                _logger.LogInformation("Invalid Day");
-                return;
-            }
-
-            _logger.LogInformation($"create image using DALL-E 3 API");
-
-            var prompt = openAiService.GetPrompt(security.RegularMarketChange > 0)
-                .Replace("[stockName]", security.ShortName)
-                .Replace("[currentPrice]", string.Format("{0:N2}", security.RegularMarketPrice))
-                .Replace("[priceChange]", string.Format("{0:N2}", security.RegularMarketChange))
-            ;
-
-            var imageByte = await openAiService.CreateImage(prompt);
-
-            var content = $"""
-            [{estMarketTime:yyyy-MM-dd}]
-            the stock price of ${symbol}
-            ${string.Format("{0:N2}", security.RegularMarketPrice)}
-            {(security.RegularMarketChange > 0 ? "+" : "")}{string.Format("{0:N2}", security.RegularMarketChange)} ({string.Format("{0:N2}", security.RegularMarketChangePercent)}%)
-            """;
-
-            _logger.LogInformation("post tweet using X API");
-            await tweetService.PostTweet(content, imageByte);
-
-            _logger.LogInformation("send slack message using Slack API");
-            await slackService.SendMessage(stock.SlackChannel, symbol, content, imageByte);
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError("Error occurred while generating tweet: {exception}", ex.Message);
         }
     }
 }
